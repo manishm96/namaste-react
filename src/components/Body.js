c416a78f7a754c93fc762ee7a9929a6fa9570186
Manish Mapakshi <manishmapakshi96@gmail.com>
2024-11-20 01:12:19 -0500

    Episode 06 (Exploring the world) part 1 source code

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
-import mockData from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  // Get restaurants from mock data
-  let { restaurants } = mockData?.card?.card?.gridElements?.infoWithStyle;
-  [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+  [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const data = await response.json();
+
+    const restaurants =
+      data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+
+    console.log("restaurants", restaurants);
+
+    setListOfRestaurants(restaurants);
+  };
 
   const filterRestaurants = () => {
     const filteredRestaurants = listOfRestaurants.filter(
@@ -14,6 +32,10 @@ const Body = () => {
     console.log("filteredRestaurants", filteredRestaurants);
     setListOfRestaurants(filteredRestaurants);
   };
+
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
